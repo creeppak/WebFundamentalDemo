1af3f3c55ae742ae4887c08b0b2e4b4d9fdbdfc9
creeppak <59798783+creeppak@users.noreply.github.com>
2026-04-23 18:04:02 +0200

    Set up Hangfire with Postgres storage in isolated hangfire schema

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,6 +1,20 @@
+using Hangfire;
+using Hangfire.PostgreSql;
 using Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("Postgres")
+    ?? throw new InvalidOperationException("Connection string 'Postgres' is not configured.");
+
+builder.Services.AddHangfire(config => config
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UsePostgreSqlStorage(c => c.UseNpgsqlConnection(connectionString),
+        new PostgreSqlStorageOptions { SchemaName = "hangfire" }));
+
+builder.Services.AddHangfireServer();
 builder.Services.AddHostedService<WorkerService>();
 
 var host = builder.Build();
